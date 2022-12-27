25ccc0bfa235af158418e7d20b4a7304c777c1ad
ANA-NURFARIDAH <anafaism@gmail.com>
2022-12-27 23:18:58 +0700

    initial commit

---
+++ src/components/Trending.js
@@ -0,0 +1,18 @@
+import { Card, Container, Row, Col, Image } from "react-bootstrap"
+import duneImage from "../assets/images/trending/dune.jpg"
+import everythingImage from "../assets/images/trending/everything.jpg"
+import infiniteImage from "../assets/images/trending/joker.jpg"
+import jokerImage from "../assets/images/trending/lightyear.jpg"
+import morbiusImage from "../assets/images/trending/morbius.jpg"
+
+
+const Trending = () => {
+    return (
+        <div>
+            <Container>
+                <br />
+                
+            </Container>
+        </div>
+    )
+}
